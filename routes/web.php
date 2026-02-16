cb28c893f10d435f80944ac4c754827ddd57b377
Kaisa <kaisavidya@gmail.com>
2026-02-16 11:22:00 +0700

    tambah semua ui

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/murid', [\App\Http\Controllers\MuridController::class, 'edit'])->name('murid.edit');
+    Route::patch('/murid', [\App\Http\Controllers\MuridController::class, 'update'])->name('murid.update');
+    Route::delete('/murid', [\App\Http\Controllers\MuridController::class, 'destroy'])->name('murid.destroy');
+});
